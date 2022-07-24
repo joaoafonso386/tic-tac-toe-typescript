c045d860f86023ea78dfbb9963be4957b7e6fddb
João Afonso <joaoafonso386@gmail.com>
2022-07-25 00:59:47 +0100

    Change tsconfig settings to support dom types, added more logic variables

--- src/compiled/main.js
+++ src/compiled/main.js
@@ -1,11 +1,14 @@
 const board = document.querySelector(".board");
-const boardRows = board === null || board === void 0 ? void 0 : board.rows;
+const boardRows = board?.rows;
 const startButton = document.querySelector(".start-button");
 const playerOne = "X";
+const isPlayerOnePlaying = false;
+const isPlayerTwoPlaying = false;
 const playerTwo = "O";
 for (let row of boardRows) {
     const cellCollection = row.children;
     for (let cell of cellCollection) {
-        console.log(cell);
+        // const typedCell: HTMLTableElement = cell; 
+        // typedCell.onclick = 
     }
 }
--- src/main.ts
+++ src/main.ts
@@ -2,14 +2,19 @@ const board: HTMLTableElement | null = document.querySelector(".board");
 const boardRows: HTMLCollection | undefined = board?.rows;
 const startButton: HTMLButtonElement = document.querySelector(".start-button");
 const playerOne: string = "X";
+const isPlayerOnePlaying: boolean = false;
+const isPlayerTwoPlaying: boolean = false;
 const playerTwo: string = "O";
 
 for(let row of boardRows) {
 
   const cellCollection: HTMLCollection = row.children;
 
-  for(let cell of cellCollection) {
-    console.log(cell)
+  for(let cell  of cellCollection) {
+    // const typedCell: HTMLTableElement = cell; 
+
+    // typedCell.onclick = 
+
   }
 
 }
