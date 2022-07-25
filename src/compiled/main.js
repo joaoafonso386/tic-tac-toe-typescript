11feef138748a5ffe60d102d8bf6ac7bef6f054d
João Afonso <joaoafonso386@gmail.com>
2022-07-26 00:21:30 +0100

    Added initial start button logic

--- src/compiled/main.js
+++ src/compiled/main.js
@@ -2,22 +2,39 @@
 const board = document.querySelector(".board");
 const startButton = document.querySelector(".start-game");
 const playerOne = "X";
-const isInGameMode = false;
+let isInGameMode = false;
 const isPlayerOnePlaying = false;
 const isPlayerTwoPlaying = false;
 const playerTwo = "O";
 //children should have a variable of its own 
 for (let { children } of board.rows) {
     for (let cell of children) {
-        cell.addEventListener("click", () => {
-        });
+        if (isInGameMode) {
+            cell.addEventListener("click", () => {
+            });
+        }
     }
 }
 startButton.onclick = () => {
+    startButton.value = "Stop";
+    const gameStartedParagraph = document.createElement("p");
+    gameStartedParagraph.className = "currently-playing";
     //check if the game is being played or not
+    if (isInGameMode) {
+        const shouldGameEnd = confirm("Do you want to end the game?");
+        if (shouldGameEnd) {
+            isInGameMode = false;
+            startButton.value = "Start";
+            startButton.nextElementSibling.remove();
+        }
+    }
+    else {
+        isInGameMode = true;
+        gameStartedParagraph.textContent = "You are currently playing!";
+        startButton.parentNode.append(gameStartedParagraph);
+    }
     //if it is not being played then allow for cells to be clicked and X's or O's being added to the board
     //also change the button text from "start" to "stop"
     //if the game is being played, then the button text should be "stop"
     //if the user clicks the stop button then a "confirm stop game" window should pop up to make clear that the user wants to stop the game no matter at what point the game is 
-    console.log("game started");
 };
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 const board: HTMLTableElement = document.querySelector(".board");
-const startButton: HTMLButtonElement = document.querySelector(".start-game");
+const startButton: HTMLInputElement = document.querySelector(".start-game");
 const playerOne: string = "X";
-const isInGameMode: boolean = false;
+let isInGameMode: boolean = false;
 const isPlayerOnePlaying: boolean = false;
 const isPlayerTwoPlaying: boolean = false;
 const playerTwo: string = "O";
@@ -11,10 +11,14 @@ for(let { children } of board.rows) {
 
   for (let cell  of children) {
 
-    cell.addEventListener("click",() => {
+    if(isInGameMode) {
 
+      cell.addEventListener("click",() => {
+  
+  
+      }) 
 
-    }) 
+    }
 
   }
 
@@ -22,12 +26,31 @@ for(let { children } of board.rows) {
 
 startButton.onclick = () => {
   
+  startButton.value = "Stop";
+  const gameStartedParagraph: HTMLParagraphElement = document.createElement("p");
+  gameStartedParagraph.className = "currently-playing";
+
+
   //check if the game is being played or not
+  if(isInGameMode) {
+    const shouldGameEnd: boolean = confirm("Do you want to end the game?");
+    if(shouldGameEnd) {
+      isInGameMode = false;
+      startButton.value = "Start";
+      startButton.nextElementSibling.remove();
+    } 
+  } else {
+    isInGameMode = true;
+    gameStartedParagraph.textContent = "You are currently playing!";
+    startButton.parentNode.append(gameStartedParagraph);
+  }
+  
+
   //if it is not being played then allow for cells to be clicked and X's or O's being added to the board
   //also change the button text from "start" to "stop"
   //if the game is being played, then the button text should be "stop"
   //if the user clicks the stop button then a "confirm stop game" window should pop up to make clear that the user wants to stop the game no matter at what point the game is 
-  
-  console.log("game started")
 }
+  
+
 
