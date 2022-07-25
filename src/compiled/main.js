2f390a15c3a77457c87d965e9a876c9626f2b995
João Afonso <joaoafonso386@gmail.com>
2022-07-25 23:32:03 +0100

    Structure with comments how the start button logic should run

--- src/compiled/main.js
+++ src/compiled/main.js
@@ -1,15 +1,23 @@
 "use strict";
 const board = document.querySelector(".board");
-const startButton = document.querySelector(".start-button");
+const startButton = document.querySelector(".start-game");
 const playerOne = "X";
+const isInGameMode = false;
 const isPlayerOnePlaying = false;
 const isPlayerTwoPlaying = false;
 const playerTwo = "O";
+//children should have a variable of its own 
 for (let { children } of board.rows) {
     for (let cell of children) {
-        console.log(cell);
         cell.addEventListener("click", () => {
-            console.log("clicked");
         });
     }
 }
+startButton.onclick = () => {
+    //check if the game is being played or not
+    //if it is not being played then allow for cells to be clicked and X's or O's being added to the board
+    //also change the button text from "start" to "stop"
+    //if the game is being played, then the button text should be "stop"
+    //if the user clicks the stop button then a "confirm stop game" window should pop up to make clear that the user wants to stop the game no matter at what point the game is 
+    console.log("game started");
+};
--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,33 @@
-const board: HTMLTableElement | null = document.querySelector(".board");
-const startButton: HTMLButtonElement | null = document.querySelector(".start-button");
+const board: HTMLTableElement = document.querySelector(".board");
+const startButton: HTMLButtonElement = document.querySelector(".start-game");
 const playerOne: string = "X";
+const isInGameMode: boolean = false;
 const isPlayerOnePlaying: boolean = false;
 const isPlayerTwoPlaying: boolean = false;
 const playerTwo: string = "O";
 
-
+//children should have a variable of its own 
 for(let { children } of board.rows) {
 
   for (let cell  of children) {
-    console.log(cell)
+
     cell.addEventListener("click",() => {
-      console.log("clicked")
+
+
     }) 
 
   }
 
+}
+
+startButton.onclick = () => {
+  
+  //check if the game is being played or not
+  //if it is not being played then allow for cells to be clicked and X's or O's being added to the board
+  //also change the button text from "start" to "stop"
+  //if the game is being played, then the button text should be "stop"
+  //if the user clicks the stop button then a "confirm stop game" window should pop up to make clear that the user wants to stop the game no matter at what point the game is 
+  
+  console.log("game started")
 }
 
