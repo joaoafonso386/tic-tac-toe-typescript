184be09fea9b95d5db80f71cbd7c214d90773b69
João Afonso <joaoafonso386@gmail.com>
2022-07-25 01:26:10 +0100

    Minor style changes. More compiler config options and event listener added to cells

--- src/compiled/main.js
+++ src/compiled/main.js
@@ -1,14 +1,15 @@
+"use strict";
 const board = document.querySelector(".board");
-const boardRows = board?.rows;
 const startButton = document.querySelector(".start-button");
 const playerOne = "X";
 const isPlayerOnePlaying = false;
 const isPlayerTwoPlaying = false;
 const playerTwo = "O";
-for (let row of boardRows) {
-    const cellCollection = row.children;
-    for (let cell of cellCollection) {
-        // const typedCell: HTMLTableElement = cell; 
-        // typedCell.onclick = 
+for (let { children } of board.rows) {
+    for (let cell of children) {
+        console.log(cell);
+        cell.addEventListener("click", () => {
+            console.log("clicked");
+        });
     }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,18 @@
 const board: HTMLTableElement | null = document.querySelector(".board");
-const boardRows: HTMLCollection | undefined = board?.rows;
-const startButton: HTMLButtonElement = document.querySelector(".start-button");
+const startButton: HTMLButtonElement | null = document.querySelector(".start-button");
 const playerOne: string = "X";
 const isPlayerOnePlaying: boolean = false;
 const isPlayerTwoPlaying: boolean = false;
 const playerTwo: string = "O";
 
-for(let row of boardRows) {
 
-  const cellCollection: HTMLCollection = row.children;
+for(let { children } of board.rows) {
 
-  for(let cell  of cellCollection) {
-    // const typedCell: HTMLTableElement = cell; 
-
-    // typedCell.onclick = 
+  for (let cell  of children) {
+    console.log(cell)
+    cell.addEventListener("click",() => {
+      console.log("clicked")
+    }) 
 
   }
 
