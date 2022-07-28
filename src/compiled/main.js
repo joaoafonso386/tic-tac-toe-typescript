f7abcb0e86dab36d52eb62f61364f054ab4c6faf
João Afonso <joaoafonso386@gmail.com>
2022-07-29 00:42:07 +0100

    Structure in notes of the initial logic for winning conditions

--- src/compiled/main.js
+++ src/compiled/main.js
@@ -6,24 +6,36 @@ const playerTwo = "O";
 let playerOnePlays = 0;
 let playerTwoPlays = 0;
 let isInGameMode = false;
-//create a function that checks the board at every play to see winning conditions 
+// Create a function that checks the board at every play to see winning conditions. Create functions for each winning condition (horizontal, vertical, diagonal, tie)
+// Check for a horizontal winner -> Check parent element cells and see if every cell has "X" or "O"
+// Check for a vertical winner -> Check if positon[1] is filled for that parent element and the other parent elements
+// Check for a diagonal winner -> Check if position[0][1][2] are filled with the same content or positions[2][1][0] of each parent element
+// Check for a tie -> all cells are filled but no horizontal, diagonal or vertical conditions are met
+const determineWinner = (cell) => {
+    const cellArr = [];
+    cellArr.push(cell);
+    console.log(cellArr);
+};
 for (let row of board.rows) {
     for (let cell of row.children) {
         const typedCell = cell;
         typedCell.style.color = "red";
         typedCell.style.textAlign = "center";
+        // console.log(typedCell)
         typedCell.addEventListener("click", () => {
             if (isInGameMode) {
                 if (playerOnePlays < playerTwoPlays || playerOnePlays === playerTwoPlays) {
                     if (typedCell.innerText.length <= 0) {
                         playerOnePlays++;
                         typedCell.innerText = playerOne;
+                        determineWinner(typedCell);
                     }
                 }
                 else {
                     if (typedCell.innerText.length <= 0) {
                         playerTwoPlays++;
                         typedCell.innerText = playerTwo;
+                        determineWinner(typedCell);
                     }
                 }
             }
--- src/main.ts
+++ src/main.ts
@@ -7,8 +7,21 @@ let playerTwoPlays: number = 0;
 let isInGameMode: boolean = false;
  
 
-//create a function that checks the board at every play to see winning conditions 
+// Create a function that checks the board at every play to see winning conditions. Create functions for each winning condition (horizontal, vertical, diagonal, tie)
+// Check for a horizontal winner -> Check parent element cells and see if every cell has "X" or "O"
+// Check for a vertical winner -> Check if positon[1] is filled for that parent element and the other parent elements
+// Check for a diagonal winner -> Check if position[0][1][2] are filled with the same content or positions[2][1][0] of each parent element
+// Check for a tie -> all cells are filled but no horizontal, diagonal or vertical conditions are met
 
+const determineWinner = (cell: HTMLTableCellElement) => {
+
+  const cellArr: HTMLTableCellElement[] = [];
+
+  cellArr.push(cell)
+
+  console.log (cellArr)
+ 
+}
 
 for(let row of board.rows) {
   for (let cell of row.children) {
@@ -16,6 +29,7 @@ for(let row of board.rows) {
     const typedCell = cell as HTMLTableCellElement;
     typedCell.style.color = "red";
     typedCell.style.textAlign = "center";
+    // console.log(typedCell)
     
     typedCell.addEventListener("click",() => { 
       if(isInGameMode) {
@@ -23,11 +37,13 @@ for(let row of board.rows) {
           if(typedCell.innerText.length <= 0) {
             playerOnePlays++;
             typedCell.innerText = playerOne;
+            determineWinner(typedCell)
           } 
         } else {
           if(typedCell.innerText.length <= 0) {
             playerTwoPlays++;
             typedCell.innerText = playerTwo;
+            determineWinner(typedCell)
           }
         }
       }
