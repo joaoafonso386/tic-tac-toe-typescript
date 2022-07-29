081ac026ce12c9647e13c2c0cd86b28fd13b9f5e
João Afonso <joaoafonso386@gmail.com>
2022-07-30 00:35:27 +0100

    Logic for horizontal winner finished

--- src/compiled/main.js
+++ src/compiled/main.js
@@ -8,14 +8,21 @@ let playerTwoPlays = 0;
 let isInGameMode = false;
 // Create a function that checks the board at every play to see winning conditions. Create functions for each winning condition (horizontal, vertical, diagonal, tie)
 // Check for a horizontal winner -> Check parent element cells and see if every cell has "X" or "O"
-// Check for a vertical winner -> Check if positon[1] is filled for that parent element and the other parent elements
+// Check for a vertical winner -> Check if position[1] is filled for that parent element and the other parent elements
 // Check for a diagonal winner -> Check if position[0][1][2] are filled with the same content or positions[2][1][0] of each parent element
 // Check for a tie -> all cells are filled but no horizontal, diagonal or vertical conditions are met
-const determineWinner = (cell) => {
-    determineHorizontalWinner(cell);
+// Stop the game if a winner has been found
+const determineWinner = () => {
+    for (let row of board.rows) {
+        determineHorizontalWinner(row);
+    }
 };
-const determineHorizontalWinner = (cell) => {
-    console.log(cell);
+const determineHorizontalWinner = (row) => {
+    const rowArray = [...row.children];
+    if (rowArray.every(el => el.innerText === playerOne))
+        return console.log(`Player ${playerOne} has won!`);
+    if (rowArray.every(el => el.innerText === playerTwo))
+        return console.log(`Player ${playerTwo} has won!`);
 };
 for (let row of board.rows) {
     for (let cell of row.children) {
@@ -29,15 +36,14 @@ for (let row of board.rows) {
                 typedCell.innerText.length <= 0) {
                 playerOnePlays++;
                 typedCell.innerText = playerOne;
-                determineWinner(typedCell);
             }
             else {
                 if (typedCell.innerText.length <= 0) {
                     playerTwoPlays++;
                     typedCell.innerText = playerTwo;
-                    determineWinner(typedCell);
                 }
             }
+            determineWinner();
         });
     }
 }
--- src/main.ts
+++ src/main.ts
@@ -9,19 +9,26 @@ let isInGameMode: boolean = false;
 
 // Create a function that checks the board at every play to see winning conditions. Create functions for each winning condition (horizontal, vertical, diagonal, tie)
 // Check for a horizontal winner -> Check parent element cells and see if every cell has "X" or "O"
-// Check for a vertical winner -> Check if positon[1] is filled for that parent element and the other parent elements
+// Check for a vertical winner -> Check if position[1] is filled for that parent element and the other parent elements
 // Check for a diagonal winner -> Check if position[0][1][2] are filled with the same content or positions[2][1][0] of each parent element
 // Check for a tie -> all cells are filled but no horizontal, diagonal or vertical conditions are met
+// Stop the game if a winner has been found
 
-const determineWinner = (cell: HTMLTableCellElement) => {
+const determineWinner = () => {
 
-  determineHorizontalWinner(cell);
+  for(let row of board.rows) {
+    determineHorizontalWinner(row)
+  }
+
  
 }
 
-const determineHorizontalWinner = (cell: HTMLTableCellElement) => {
+const determineHorizontalWinner = (row: HTMLTableRowElement) => {
 
-  console.log(cell)
+  const rowArray = [...row.children] as HTMLTableCellElement[];
+
+  if(rowArray.every(el => el.innerText === playerOne)) return console.log(`Player ${playerOne} has won!`)
+  if(rowArray.every(el => el.innerText === playerTwo)) return console.log(`Player ${playerTwo} has won!`)
 
 }
 
@@ -35,20 +42,23 @@ for(let row of board.rows) {
     
     typedCell.addEventListener("click",() => { 
         if(!isInGameMode) return
-        
+
         if((playerOnePlays < playerTwoPlays || playerOnePlays === playerTwoPlays) && 
         typedCell.innerText.length <= 0) {
           playerOnePlays++;
           typedCell.innerText = playerOne;
-          determineWinner(typedCell)
         } else {
           if(typedCell.innerText.length <= 0) {
             playerTwoPlays++;
             typedCell.innerText = playerTwo;
-            determineWinner(typedCell)
           }
-       }
-    })
+        }
+        determineWinner()
+      })
+
+      
+    
+
   }
 }
 
