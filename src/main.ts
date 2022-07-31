17966d3e39a496bdc98d44de23176fafdadb43f0
João Afonso <joaoafonso386@gmail.com>
2022-07-31 18:33:17 +0100

    Organize project notes

--- src/main.ts
+++ src/main.ts
@@ -7,13 +7,7 @@ let playerTwoPlays: number = 0;
 let isInGameMode: boolean = false;
  
 
-// Create a function that checks the board at every play to see winning conditions. Create functions for each winning condition (horizontal, vertical, diagonal, tie)
-// Check for a horizontal winner -> Check parent element cells and see if every cell has "X" or "O"
-// Check for a vertical winner -> Check if position[1] is filled for that parent element and the other parent elements
-// Check for a diagonal winner -> Check if position[0][1][2] are filled with the same content or positions[2][1][0] of each parent element
-// Check for a tie -> all cells are filled but no horizontal, diagonal or vertical conditions are met
-// Stop the game if a winner has been found
-//Add return types to functions
+
 
 const determineWinner = () => {
 
@@ -27,7 +21,7 @@ const determineWinner = () => {
  
 }
 
-const determineHorizontalWinner = (rowArray: HTMLTableCellElement[]) => {
+const determineHorizontalWinner = (rowArray: HTMLTableCellElement[]): void => {
 
   if(rowArray.every(el => el.innerText === playerOne)) return console.log(`Player ${playerOne} has won!`)
   if(rowArray.every(el => el.innerText === playerTwo)) return console.log(`Player ${playerTwo} has won!`)
