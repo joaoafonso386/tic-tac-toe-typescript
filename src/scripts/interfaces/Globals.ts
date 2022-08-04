f45292676f5cfb6ef8b565241600797d4cb70e85
João Afonso <joaoafonso386@gmail.com>
2022-08-04 01:03:29 +0100

    Added Global interface into individual file

---
+++ src/scripts/interfaces/Globals.ts
@@ -0,0 +1,27 @@
+interface Globals {
+
+  DOM: {
+    board?: HTMLTableElement,
+    startButton?: HTMLInputElement,
+    gameStartedParagraph?: HTMLParagraphElement,
+    infoParagraph?: HTMLParagraphElement,
+    whoIsPlayingParagraph: HTMLLIElement,
+    cellArray: HTMLTableCellElement[]
+  },
+  players: {
+    playerOne: string,
+    playerTwo: string
+  }
+  plays?: {
+    playerOnePlays: number,
+    playerTwoPlays: number
+  }
+  winningConditions: number[][]
+  controlVariables: {
+    isInGameMode?: boolean,
+    winnerIsFound: boolean
+  }
+
+}
+
+export { Globals }
